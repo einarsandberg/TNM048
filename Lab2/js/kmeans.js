ea915307d92c4f6a7373ff168c9fb2db90a23002
einarsandberg <einars92@gmail.com>
2016-02-02 17:29:30 +0100

    hehheehhee

--- Lab2/js/kmeans.js
+++ Lab2/js/kmeans.js
@@ -7,6 +7,7 @@
    
     function kmeans(data, k) 
     {
+        
         var quality = 0;
         var newQuality = 0;
         var iterate = false;
@@ -44,9 +45,9 @@
         var distance;
         var distanceArray;
         var minArray = [];
-        var counter = 0;
+        //var counter = 0;
         var minVal;
-        var bestClusterIndexArray = [];
+        var bestCentroidIndexArray = [];
         var newCentroids = [];
         var iterations = 0;
         do
@@ -56,10 +57,10 @@
             var minArray = [];
 
             var minVal;
-            var bestClusterIndexArray = [];
-            newCentroids = [];
+            var bestCentroidIndexArray = [];
+
             // Step 2
-            if (iterations <1)
+            if (iterations < 1)
             {
                 for (var i = 0; i < data.length; i++)
                 {
@@ -67,89 +68,137 @@
                     distanceArray = [];
                     for (var j = 0; j < k; j++)
                     {
-                        distance = (Math.pow(data[i]["A"] - listOfRandoms[j][0], 2)) +
-                                    (Math.pow(data[i]["B"] - listOfRandoms[j][1], 2)) + 
-                                    (Math.pow(data[i]["C"] - listOfRandoms[j][2], 2));
+                        distance = (Math.pow(parseFloat(data[i]["A"]) - listOfRandoms[j][0], 2)) +
+                                    (Math.pow(parseFloat(data[i]["B"]) - listOfRandoms[j][1], 2)) + 
+                                    (Math.pow(parseFloat(data[i]["C"]) - listOfRandoms[j][2], 2));
                         distanceArray.push(Math.sqrt(distance));
+                       
                     }
-                    // save the index of the best cluster.
+                    console.log(distanceArray);
+                    // save the index of the best centroid.
                     // the value will correspond to the index in the random array.
-                    bestClusterIndexArray.push(distanceArray.indexOf(Math.min.apply(Math, distanceArray)));
+                    bestCentroidIndexArray.push(distanceArray.indexOf(Math.min.apply(Math, distanceArray)));
                     distance = 0;
                     distanceArray = [];
+                    
                 }
+                console.log(bestCentroidIndexArray);
             }
             else
             {
-
                 for (var i = 0; i < data.length; i++)
                 {
                     distance = 0;
                     distanceArray = [];
-                    for (var j = 0; j < k; j++)
+                    for (var j = 0; j < newCentroids.length; j++)
                     {
-                        distance = (Math.pow(data[i]["A"] -  newCentroids[j][0], 2)) +
-                                    (Math.pow(data[i]["B"] - newCentroids[j][1], 2)) + 
-                                    (Math.pow(data[i]["C"] - newCentroids[j][2], 2));
+                      //  console.log(newCentroids);
+                        distance = (Math.pow(parseFloat(data[i]["A"]) -  newCentroids[j][0], 2)) +
+                                    (Math.pow(parseFloat(data[i]["B"]) - newCentroids[j][1], 2)) + 
+                                    (Math.pow(parseFloat(data[i]["C"]) - newCentroids[j][2], 2));
                         distanceArray.push(Math.sqrt(distance));
+                        
                     }
+
+                    //console.log(distanceArray);
                     // save the index of the best cluster.
                     // the value will correspond to the index in the random array.
-                    bestClusterIndexArray.push(distanceArray.indexOf(Math.min.apply(Math, distanceArray)));
+                   // console.log(distance);
+                  // console.log(distanceArray);
+                    bestCentroidIndexArray.push(distanceArray.indexOf(Math.min.apply(Math, distanceArray)));
+                    //console.log(distanceArray);
                     distance = 0;
                     distanceArray = [];
+
                 }
             }
+           // console.log(newCentroids);
+            //console.log(bestCentroidIndexArray);
        
             
             // Step 3
 
             /* recalculate centroids by computing the average of all data points 
                assigned to each centroid */
-            var avgA = [0,0];
-            var avgB = [0,0];
-            var avgC = [0,0];
-            var counter = [0,0];
+            var avgA = [];
+            var avgB = [];
             
-            for (var i = 0; i < bestClusterIndexArray.length; i++)
-            {
-                
-                avgA[bestClusterIndexArray[i]] += parseFloat(data[i]["A"]);
-                avgB[bestClusterIndexArray[i]] += parseFloat(data[i]["B"]);
-                avgC[bestClusterIndexArray[i]] += parseFloat(data[i]["C"]);
-                counter[bestClusterIndexArray[i]] = counter[bestClusterIndexArray[i]] + 1;
-               
-                if (i == bestClusterIndexArray.length - 1)
-                {
-                    for (var j = 0; j < k; j++)
-                    {
-                        avgA[j] = avgA[j]/counter[j];
-                        avgB[j] = avgB[j]/counter[j];
-                        avgC[j] = avgC[j]/counter[j];
-                        newCentroids.push( [avgA[j], avgB[j], avgC[j]]);
-                    }
+            var avgC = [];
+            var counter = [];
 
+
+            var count = 0;
+           // console.log(bestCentroidIndexArray);
+            for (var i = 0; i < bestCentroidIndexArray.length; i++)
+            {
+                if (bestCentroidIndexArray.includes(i))
+                {
+                    count++;
+                    console.log("HEJJJJJ");
                 }
             }
-            
+            //console.log(count);
+            for (var i = 0; i < count; i++)
+            {
+                counter.push(0);
+                avgA.push(0);
+                avgB.push(0);
+                avgC.push(0);
+
+            }
+
+            newCentroids = [];
+            //console.log(bestCentroidIndexArray);
+            //console.log(counter);
+            for (var i = 0; i < bestCentroidIndexArray.length; i++)
+            {
+                
+                avgA[bestCentroidIndexArray[i]] += parseFloat(data[i]["A"]);
+                avgB[bestCentroidIndexArray[i]] += parseFloat(data[i]["B"]);
+                avgC[bestCentroidIndexArray[i]] += parseFloat(data[i]["C"]);
+                counter[bestCentroidIndexArray[i]] = counter[bestCentroidIndexArray[i]] + 1;
+                
+                if (i == bestCentroidIndexArray.length - 1)
+                {
+
+                    for (var j = 0; j < counter.length; j++)
+                    {
+                            avgA[j] = avgA[j]/counter[j];
+                            avgB[j] = avgB[j]/counter[j];
+                            avgC[j] = avgC[j]/counter[j];
+                        
+
+                        newCentroids.push([avgA[j], avgB[j], avgC[j]]);
+                        //console.log(newCentroids);
+                      // console.log(newCentroids);
+                        
+                    }
+                        //console.log(counter);
+                        
+
+
+                }
+            }          
+            //console.log(newCentroids);
 
             // Step 4
             var qualityArray = [];
         
         
-            for (var i = 0; i < bestClusterIndexArray.length; i++)
+            for (var i = 0; i < bestCentroidIndexArray.length; i++)
             {
                 if (iterations == 0)
                 {
+                   // console.log(newCentroids);
                    // console.log("HEJ");
                     quality += Math.pow(parseFloat(data[i]["A"]) - 
-                    newCentroids[bestClusterIndexArray[i]][0], 2) + 
+                    newCentroids[bestCentroidIndexArray[i]][0], 2) + 
                     Math.pow(parseFloat(data[i]["B"]) - 
-                    newCentroids[bestClusterIndexArray[i]][1], 2) + 
+                    newCentroids[bestCentroidIndexArray[i]][1], 2) + 
                     Math.pow(parseFloat(data[i]["C"]) -
-                    newCentroids[bestClusterIndexArray[i]][2], 2);
+                    newCentroids[bestCentroidIndexArray[i]][2], 2);
                     iterate = true;
-                    if (i == bestClusterIndexArray.length -1)
+                    if (i == bestCentroidIndexArray.length -1)
                     {
                         iterations++;
                     }
@@ -161,19 +210,19 @@
                    // console.log("TJU");
                     
                     newQuality += Math.pow(parseFloat(data[i]["A"]) - 
-                    newCentroids[bestClusterIndexArray[i]][0], 2) + 
+                    newCentroids[bestCentroidIndexArray[i]][0], 2) + 
                     Math.pow(parseFloat(data[i]["B"]) - 
-                    newCentroids[bestClusterIndexArray[i]][1], 2) + 
+                    newCentroids[bestCentroidIndexArray[i]][1], 2) + 
                     Math.pow(parseFloat(data[i]["C"]) -
-                    newCentroids[bestClusterIndexArray[i]][2], 2);
-                    if (i == bestClusterIndexArray.length -1)
+                    newCentroids[bestCentroidIndexArray[i]][2], 2);
+                    if (i == bestCentroidIndexArray.length -1)
                     {
                         iterations++;
                     }
                 }
             }
-            console.log(quality);
-            console.log(newQuality);
+           /* console.log(quality);
+            console.log(newQuality);*/
             if (iterations <= 1)
             {
                 iterate = true;
@@ -188,8 +237,10 @@
                 iterate = false;
             }
 
-
+            //console.log(newCentroids);
         }while(iterate);
+
+
 
    
         
